8efba0883bc118963b66493d1f673eaa008f6485
hisyamsz <hisyam.s901@gmail.com>
2026-07-20 22:07:51 +0700

    feat: implement dark mode support with theme provider and toggle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { DarkmodeToggle } from "@/components/common/darkmode-toggle";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Realtime POS Apps",
+  description: "",
+};
 
 export default function Home() {
   return (
     <div>
       <Input />
       <Button>Hello</Button>
+      <DarkmodeToggle />
     </div>
   );
 }
